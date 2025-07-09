5f8137d65b1d231c0d7e39cbd7c14446c3deb3c0
Stefan Meinecke <meinecke@greensec.de>
2025-07-09 20:23:11 +0200

    add build

--- check_requirements.sh
+++ check_requirements.sh
@@ -4,29 +4,29 @@
 
 #  pkg install p5-JSON p5-LWP-Protocol-https p5-File-Slurp
 
-for i in lib/*.pm *.pl ; do
-	if ! perl -c "$i" ; then echo "Please FIX" ; exit 99; fi
+for i in lib/*.pm *.pl; do
+	if ! perl -c "$i"; then
+		echo "Please FIX"
+		exit 99
+	fi
 done
 
-
-
 MISSING=""
-for tool in git pdftocairo pngtopam convert pbmtog3 html2text ; do
-	if ! which "$tool" ; then
-		echo "Executable not found: $tool";
+for tool in git pdftocairo pngtopam convert pbmtog3 html2text; do
+	if ! which "$tool"; then
+		echo "Executable not found: $tool"
 		MISSING="$MISSING $tool"
 	fi
 done
-if [  -n "$MISSING" ]; then
+if [ -n "$MISSING" ]; then
 
-# Sometimes this is needed...
-  #  pkg search netpbm
-  #  pkg install -y netpbm
-  #  pkg install -y poppler
-  #  pkg install -y ImageMagick7-nox11
-  #  pkg install -y html2text
+	# Sometimes this is needed...
+	#  pkg search netpbm
+	#  pkg install -y netpbm
+	#  pkg install -y poppler
+	#  pkg install -y ImageMagick7-nox11
+	#  pkg install -y html2text
 
 	echo "Install some packages... for $MISSING"
 	exit 99
 fi
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,21 @@ echo $LD_LIBRARY_PATH
 cd /IPPFritzFax
 #set -vx
 for i in "$@"; do
- set X $(echo $i | sed 's,=, ,')
- shift;
- case $1 in
-   -pass*) echo password=$2;;
-   -tel*)  echo telFrom=$2;;
-   -user*) echo user=$2;;
-   -url*)  echo url=$2;;
-   *) echo "Unown argument '$1'" >&2;
-	exit 99;
-	;;
- esac
+  set X $(echo $i | sed 's,=, ,')
+  shift
+  case $1 in
+    -pass*) echo password=$2 ;;
+    -tel*) echo telFrom=$2 ;;
+    -user*) echo user=$2 ;;
+    -url*) echo url=$2 ;;
+    *)
+      echo "Unown argument '$1'" >&2
+      exit 99
+    ;;
+  esac
 done >~/.credentials
 dbus-daemon --system
 avahi-daemon -D
 avahi-dnsconfd -D
 
 ippserver -C faxserver -K crt -d spool -r _universal
-
